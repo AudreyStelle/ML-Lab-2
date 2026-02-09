10a0c970db78757809568a89de6c12ec548b4a56
Audrey Stelle <jtr6rd@virginia.edu>
2026-02-09 15:31:49 -0500

    Update MLLab2Work.py

--- MLLab2Work.py
+++ MLLab2Work.py
@@ -1,1 +1,2 @@
-
+# 1 Use the question/target variable you submitted and build a model to answer the question you created for this dataset (make sure it is a classification problem, convert if necessary).
+#My Question: How does student financial need impact college completion rates?
